fcfbd037e5574e5d8867d44ef3c56678552d9696
Jacob Smasal <jacobsmasal@Jacobs-MacBook-Pro-126.local>
2024-05-02 20:34:08 -0500

    DDT

---
+++ R/DDT.R
@@ -0,0 +1,13 @@
+#' DDT data set for fish
+#'
+#' 144 lines of measurements on fish
+#'
+#' @format A data of 144 rows and 5 variables:
+#' \describe{
+#'   \item{LENGTH}{length of fish}
+#'   \item{WEIGHT}{weight of fish}
+#'   \item{year}{Year}
+#'   ...
+#' }
+#' @source text book
+"ddt"
